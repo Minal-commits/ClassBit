f3df06114a736d1b7367f26e19961e548fba47a6
Minal-commits <minalranjit.dev@gmail.com>
2025-07-31 14:50:25 +0530

    changed the profile text

--- src/Screens/Student/StudentHeader.jsx
+++ src/Screens/Student/StudentHeader.jsx
@@ -4,7 +4,7 @@ import CustomButton from '../components/CustomButton'
 const Studentheader = ({studentName}) => {
   return (
     <div className='w-full flex flex-col justify-center items-center gap-6 pt-5'>
-        <p className='text-4xl mt-2 font-semibold'>Welcome, <span className='text-red-500'>{studentName}!</span> See how your students are doing. 👋</p>
+        <p className='text-4xl mt-2 font-semibold'>Welcome, <span className='text-red-500'>{studentName}!</span> 👋</p>
     </div>
   )
 }
