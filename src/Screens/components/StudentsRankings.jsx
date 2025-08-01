5d9cc876907f1b53a9cb9c8c4471a7c8adc72518
Minal-Commits <you@example.com>
2025-08-01 21:21:06 +0530

    Added the rankings page

--- src/Screens/components/StudentsRankings.jsx
+++ src/Screens/components/StudentsRankings.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const StudentsRankings = ({students="Student"}) => {
+const StudentsRankings = ({students}) => {
+  useEffect(()=> {
+    console.log(students);
+  })
   return (
     <div className='w-full bg-green-200 mt-8 min-h-[65vh] rounded-md my-5'>
       <div className='w-full flex justify-between text-xl px-10 py-3 bg-amber-400 rounded-t-md'>
@@ -12,8 +15,8 @@ const StudentsRankings = ({students="Student"}) => {
       <div className='w-full flex flex-col justify-between text-xl px-10 py-3'>
       {students && students.map((student, index) => {
         return (
-          <div key={index} className="flex justify-between text-sm py-3">
-            <p className='min-w-[25%]'>{student.username}</p>
+          <div key={index} className={`flex justify-between text-sm py-3 ${(index === 0) ? "font-bold" : ""}`}>
+            <p className={`min-w-[25%]`}>{student.username} {(index === 0) ? "(1st)" : ""}</p>
             <p className='min-w-[25%] '>{student.points}</p>
             <p className='min-w-[25%] '>{student.year}</p>
             <p className='min-w-[25%] '>{student.number_of_solved_problems}</p>
